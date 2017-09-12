9980c06754892676e64e455b989059e299293f9c
FHV <floh@flohdot.org>
2017-09-12 15:44:52 -0400

    chore(ui): ToS override

---
+++ file-overrides/src/ui/shared-components/Terms.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable max-len */
+const React = require('react');
+
+class Terms extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Expandoo Terms of Use</h1>
+                <p>Please see <a href="http://cmd.solutions/en/legal-note/">our website</a>.</p>
+            </div>
+        );
+    }
+}
+
+module.exports = Terms;
